21ab8c844a38f3d65816b066432e2793c8b6f343
Yosua Tarigan <yosuatarigan7@gmail.com>
2020-05-09 17:02:03 +0700

    'ol'

--- src/Kegiatan/NoteKegiatan.js
+++ src/Kegiatan/NoteKegiatan.js
@@ -9,7 +9,7 @@ const NoteKegiatan = () => {
         <div >
             <article>
                 <header className="tc b--black-70 pv4">
-                    <h3 className="f2 fw7 ttu tracked lh-title mt0 mb2 avenir">Kegiatan diSekolah</h3>
+                    <h3 className="f2 fw7 ttu tracked lh-title mt0 mb2 avenir">Kegiatan di Sekolah</h3>
                     {/* <h4 class="f3 fw4 i lh-title mt0">Siem Reap, Cambodia</h4> */}
                 </header>
                 <Flag kegiatan1 = {Kegiatan1}/>
